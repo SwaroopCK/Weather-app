4b5ceebc7fe4eb0bdbaee07bc633f42539f82f22
SwaroopCK <swaroopkorde@gmail.com>
2020-11-29 15:31:49 +0530

    Final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
@@ -12,8 +12,8 @@ weather.temperature = {
 }
 
 const KELVIN = 273;
-
-const key = "82005d27a116c2880c8f0fcb866998a0";
+//WeatherMap api key
+const key = "992550205b12100d63973a93c93de4e6";
 
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition , showError);
@@ -39,7 +39,28 @@ function showError(error){
 
 //getWeather() from APi
 function getWeather(latitude,longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?
-    lat=${latitude}&long=${longitude}&appid=$(key)`;
-    console.log(api);
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].icon;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        })
+}
+
+//
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
